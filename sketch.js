6997bf4457179fcc01c45d5bac72dffc3af38ea3
Jeffrey Arts <sjeffff@gmail.com>
2024-10-20 19:48:11 +0200

    Simplify code

--- sketch.js
+++ sketch.js
@@ -1,40 +1,7 @@
-//////////////////////////////////////////////////
-// Object for creation and real-time resize of canvas
-// Good function to create canvas and resize functions. I use this in all examples.
-const C = {
-  loaded: false,
-  prop() {return this.height/this.width},
-  isLandscape() {return window.innerHeight <= window.innerWidth * this.prop()},
-  resize () {
-    if (this.isLandscape()) {
-      document.getElementById(this.css).style.height = "100%";
-      document.getElementById(this.css).style.removeProperty('width');
-    } else {
-      document.getElementById(this.css).style.removeProperty('height');
-      document.getElementById(this.css).style.width = "100%";
-    }
-  },
-  setSize(w,h,p,css) {
-    this.width = w, this.height = h, this.pD = p, this.css = css;
-  },
-  createCanvas() {
-    this.main = createCanvas(this.width,this.height,WEBGL), pixelDensity(this.pD), this.main.id(this.css), this.resize();
-  }
-};
-function windowResized () {
-  C.resize();
-}
-
-C.setSize(1300, 1900, .4, 'mainCanvas')
-//
-// End set-up
-//////////////////////////////////////////////////
-
 function setup () {
-  C.createCanvas()
-  angleMode(DEGREES)
+  createCanvas(1300, 1900,WEBGL)
+  pixelDensity(.4)
   translate(-width/2,-height/2)  
-  
   
   // Deze waardes kun je eenvoudig veranderen
   // Lees hier meer over p5's random functie: https://p5js.org/reference/p5/random/
